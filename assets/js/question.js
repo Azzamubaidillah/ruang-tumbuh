01e04fe27f28c0cf1aa6f25b8076fc398c138183
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-08-26 15:34:57 +0700

    result page

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,60 +1,114 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
 let acceptingAnswers = true
+let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
-
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
+        question: 'Apa kamu suka bersosialisasi?',
+        choice1: 'saya ingin menghindar',
+        choice2: 'saya senang sendiri',
+        choice3: 'ya saya senang keluar',
+        choice4: 'ya jika dengan orang yang saya kenal',
         answer: 2,
     },
     {
-        question:
-            "The tallest building in the world is located in which city?",
-        choice1: "Dubai",
-        choice2: "New York",
-        choice3: "Shanghai",
-        choice4: "None of the above",
+        question:"Apa peran kamu dalam keluarga?",
+        choice1: "si pendiam",
+        choice2: "orang yang sensibel",
+        choice3: "bahagia / tukang sosial",
+        choice4: "tidak punya peran",
         answer: 1,
     },
     {
-        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
-        choice1: "20%",
-        choice2: "18%",
-        choice3: "7%",
-        choice4: "33%",
+        question: "Mana yang paling sesuai menurut kamu?",
+        choice1: "hidup adalah perjuangan dan setelah berjuang lebih banyak bertahan hidup, akhirnya mati",
+        choice2: "masa masa sulit mengungkapkan siapa teman sejati",
+        choice3: "kekhawatiran lebih produktif dari kunyahan permen karet",
+        choice4: "selalu melihat sisi terang dalam kehidupan",
         answer: 3,
     },
     {
-        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
-        choice1: "10-20%",
-        choice2: "5-10%",
-        choice3: "15-20%",
-        choice4: "30%-40%",
+        question: "Bagaimana kamu menghabiskan waktu luang?",
+        choice1: "mendengarkan musik",
+        choice2: "bertemu teman dan saudara ",
+        choice3: "memikirkan sesuatu",
+        choice4: "saya senang menonton film horror",
         answer: 1,
+    }, 
+    {
+        question: "Apakah kamu dikontrol oleh emosi mu?",
+        choice1: "ya, bisa dibilang seperti itu",
+        choice2: "saya tidak terlalu punya emosi",
+        choice3: "sering, setiap saat",
+        choice4: "tidak seperti itu",
+        answer: 3,
+    }, 
+    {
+        question: "Apa kamu berpikir kamu punya masalah kesehatan mental?",
+        choice1: "saya merasa akhir akhir ini selalu bersedih",
+        choice2: "saya khawatir bahwa saya terlalu cemas",
+        choice3: "saya rasa saya anti sosial",
+        choice4: "saya rasa tidak",
+        answer: 3,
+    }, 
+    {
+        question: "Bagaimana orang lain mendeskripsikan kamu?",
+        choice1: "bahagia",
+        choice2: "tidak bahagia",
+        choice3: "canggung bersosialisasi",
+        choice4: "dingin",
+        answer: 3,
+    }, 
+    {
+        question: "Pilih salah satu harapan dibawah ini",
+        choice1: "lebih menjadi diri sendiri ",
+        choice2: "selalu bahagia seperti ini",
+        choice3: "dapat berhenti khawatir",
+        choice4: "tidak merasa puas",
+        answer: 3,
+    }, 
+    {
+        question: "Apa kamu mencintai kehidupanmu?",
+        choice1: "tidak terlalu",
+        choice2: "ya, saya mencintai hidup saya ",
+        choice3: "ya, tapi saya berharap lebih mudah dari ini ",
+        choice4: "saya sedang mencoba untuk mencintai kehidupan saya",
+        answer: 3,
+    }, 
+    {
+        question: "Mana yang paling sesuai menurut kamu?",
+        choice1: "hidup adalah perjuangan dan setelah berjuang lebih banyak bertahan hidup, akhirnya mati ",
+        choice2: "masa masa sulit mengungkapkan siapa teman sejati",
+        choice3: "kekhawatiran lebih produktif dari kunyahan permen karet",
+        choice4: "selalu melihat sisi terang dalam kehidupan",
+        answer: 3,
     }
+    
 ]
 
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 10
 
-startQuiz = () => {
+startGame = () => {
     questionCounter = 0
+    score = 0
     availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('../test_kesepian/end.html')
     }
 
     questionCounter++
@@ -75,7 +129,6 @@ getNewQuestion = () => {
     acceptingAnswers = true
 }
 
-
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers) return
@@ -86,10 +139,22 @@ choices.forEach(choice => {
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
 
-        }, 1000)
+        }, 0)
     })
 })
+
+incrementScore = num => {
+    score +=num
+}
+
+startGame()
